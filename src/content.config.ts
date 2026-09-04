e9d2aee8105a03c418d96d362a13543b134794d0
mzeekelly4024-web <mzeekelly4024@gmail.com>
2026-09-05 01:00:42 +0300

    Fix hero image path

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,24 +1,22 @@
-import { glob } from "astro/loaders";
 import { defineCollection } from "astro:content";
-import z from "astro/zod";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
-const blogsCollection = defineCollection({
-    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blogs" }),
-    schema: ({ image }) => z.object({
-        title: z.string(),
-        description: z.string(),
-        pubDate: z.date(),
-        author: z.string().optional(),
-        tags: z.array(z.string()).optional(),
-        draft: z.boolean().optional(),
-        image: z.object({
-            url: image(),
-            alt: z.string().optional()
-        })
-            .optional(),
+const blogs = defineCollection({
+  loader: glob({
+    base: "./src/content/blogs",
+    pattern: "**/*.{md,mdx}",
+  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: z.optional(image()),
     }),
 });
 
 export const collections = {
-    blogs: blogsCollection,
+  blogs,
 };
